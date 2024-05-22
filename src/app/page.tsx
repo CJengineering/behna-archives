c78b57ab10588194778b888081d55325eebd7d95
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-05-22 16:47:00 +0100

    8 desktop 4 mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,15 @@ import { ReactSVG } from "react-svg";
 import nextLogo from "../../public/next.svg";
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowRightIcon } from '@heroicons/react/20/solid'
-import { EyeIcon } from '@heroicons/react/24/outline'
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { EyeIcon } from "@heroicons/react/24/outline";
 
 import LogoCommunity from "@/components/LogoCommunity";
 import { getDataMax4 } from "@/lib/getDataMax4";
 
 export default async function Home() {
   const data = await getDataMax4();
+
   const products = transformDataToProducts(data);
   return (
     <div>
@@ -33,92 +34,122 @@ export default async function Home() {
         </div>
         <div className="w-full px-4 md:max-w-[1700px] items-center mx-auto   py-12">
           <div className="masonry-grid" style={{ maxWidth: "100%" }}>
-            {products
-              .filter((product) => product.imageSrc)
-              .map((product) => (
-                <Link
-                  key={product.id}
-                  href={`collection/${product.slug}`}
-                  className="masonry-item group block"
-                >
-                  <div className="w-full overflow-hidden rounded-lg">
-                    <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
-                      className="w-full h-auto object-cover object-center hover:scale-105 transform transition-transform duration-150"
-                    />
-                  </div>
-               
-                </Link>
-              ))}
+            <div className="block md:hidden">
+              {products
+                .filter((product, index) => product.imageSrc && index < 4)
+                .map((product) => (
+                  <Link
+                    key={product.id}
+                    href={`collection/${product.slug}`}
+                    className="masonry-item group block"
+                  >
+                    <div className="w-full overflow-hidden rounded-lg">
+                      <img
+                        src={product.imageSrc}
+                        alt={product.imageAlt}
+                        className="w-full h-auto object-cover object-center hover:scale-105 transform transition-transform duration-150"
+                      />
+                    </div>
+                  </Link>
+                ))}
+            </div>
+
+            <div className="hidden md:block">
+              {products
+                .filter((product) => product.imageSrc)
+                .map((product) => (
+                  <Link
+                    key={product.id}
+                    href={`collection/${product.slug}`}
+                    className="masonry-item group block"
+                  >
+                    <div className="w-full overflow-hidden rounded-lg">
+                      <img
+                        src={product.imageSrc}
+                        alt={product.imageAlt}
+                        className="w-full h-auto object-cover object-center hover:scale-105 transform transition-transform duration-150"
+                      />
+                    </div>
+                  </Link>
+                ))}
+            </div>
           </div>
         </div>
       </section>
       <section className=" mx-auto">
         <div className=" py-8 flex justify-center align-middle">
-        <Link href={'/collection'}>
-        <button
-        type="button"
-        className="inline-flex items-center gap-x-2 mt-4 rounded-md plexMono bg-black px-6 py-4 text-sm text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-      >
-        Explore
-        <EyeIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-      </button>          
-      
-        </Link>
+          <Link href={"/collection"}>
+            <button
+              type="button"
+              className="inline-flex items-center gap-x-2 mt-4 rounded-md plexMono bg-black px-6 py-4 text-sm text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+            >
+              Explore
+              <EyeIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
+            </button>
+          </Link>
         </div>
       </section>
       <section className="max-w-[1700px] mx-auto">
-  <div className="my-4 border-b border-gray-300"></div>
-  <div className="grid grid-cols-1 px-4 py-8 md:grid-cols-2 gap-4">
-    <div className="text-column">
-      <h2 className="text-3xl font-bold m-0 text-gray-900 font-ibmSans">
-        What are Behna Archives?
-      </h2>
-      <p className="py-2">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et ullam placeat, ab deleniti porro expedita cumque? Nesciunt, non exercitationem sapiente iste pariatur placeat tempora accusamus optio sed quas quos similique.
-      </p>
-      <p className="py-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum cumque a, iure architecto molestiae harum dignissimos, suscipit necessitatibus impedit obcaecati recusandae voluptatum esse deserunt magni quam corporis? Est, magnam sit.
-      </p>
-      <p className="py-2">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis ipsum placeat laudantium, nisi, maiores illum tempora fugiat nulla officia dicta a ut consequuntur, libero hic? Atque quaerat esse quas ratione.
-      </p>
-      <div className="w-auto">
-        <Link href={'/about'}>
-        <button
-        type="button"
-        className="inline-flex items-center gap-x-2 mt-4 rounded-md plexMono bg-black px-6 py-4 text-sm text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-      >
-        Learn more
-        <ArrowRightIcon className="-mr-0.5 h-5 w-5" aria-hidden="true" />
-      </button>          
-        </Link>
-      </div>
-    </div>
-    <div className="column-image hidden md:flex justify-end">
-      <img
-        src="https://storage.googleapis.com/behna-archives/00_THUMBNAILS/37_UAF_1947_Cairo%20Baghdad_THUMBNAIL.webp"
-        alt=""
-        className="self-end"
-      />
-    </div>
-  </div>
-  <div className="my-4 px-24 border-b border-gray-300"></div>
-</section>
-<section className="px-4 md:max-w-[1700px] mx-auto pt-4 pb-24 font-ibmMono">
-<h2 className="text-3xl font-bold m-0 text-gray-900 font-ibmSans mb-2">
-        Supported by 
-      </h2>
-  <div className="flex flex-col md:flex-row md:space-x-12">
-    <Image src={cjLogo} alt="" className="py-4 w-48" />
-    <Image src={factumLogo} alt="" className="py-4 w-64" />
-    <Image src={fonazione} alt="" className="py-4 w-52" />
-    <Image src={behnaLogo} alt="" className="py-4 w-36" />
-  </div>
-</section>
-
-
+        <div className="my-4 border-b border-gray-300"></div>
+        <div className="grid grid-cols-1 px-4 py-8 md:grid-cols-2 gap-4">
+          <div className="text-column">
+            <h2 className="text-3xl font-bold m-0 text-gray-900 font-ibmSans">
+              What are Behna Archives?
+            </h2>
+            <p className="py-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et ullam
+              placeat, ab deleniti porro expedita cumque? Nesciunt, non
+              exercitationem sapiente iste pariatur placeat tempora accusamus
+              optio sed quas quos similique.
+            </p>
+            <p className="py-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+              cumque a, iure architecto molestiae harum dignissimos, suscipit
+              necessitatibus impedit obcaecati recusandae voluptatum esse
+              deserunt magni quam corporis? Est, magnam sit.
+            </p>
+            <p className="py-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
+              ipsum placeat laudantium, nisi, maiores illum tempora fugiat nulla
+              officia dicta a ut consequuntur, libero hic? Atque quaerat esse
+              quas ratione.
+            </p>
+            <div className="w-auto">
+              <Link href={"/about"}>
+                <button
+                  type="button"
+                  className="inline-flex items-center gap-x-2 mt-4 rounded-md plexMono bg-black px-6 py-4 text-sm text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                >
+                  Learn more
+                  <ArrowRightIcon
+                    className="-mr-0.5 h-5 w-5"
+                    aria-hidden="true"
+                  />
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className="column-image hidden md:flex justify-end">
+            <img
+              src="https://storage.googleapis.com/behna-archives/00_THUMBNAILS/37_UAF_1947_Cairo%20Baghdad_THUMBNAIL.webp"
+              alt=""
+              className="self-end"
+            />
+          </div>
+        </div>
+        <div className="my-4 px-24 border-b border-gray-300"></div>
+      </section>
+      <section className="px-4 md:max-w-[1700px] mx-auto pt-4 pb-24 font-ibmMono">
+        <h2 className="text-3xl font-bold m-0 text-gray-900 font-ibmSans mb-2">
+          Supported by
+        </h2>
+        <div className="flex flex-col md:flex-row md:space-x-12">
+          <Image src={cjLogo} alt="" className="py-4 w-48" />
+          <Image src={factumLogo} alt="" className="py-4 w-64" />
+          <Image src={fonazione} alt="" className="py-4 w-52" />
+          <Image src={behnaLogo} alt="" className="py-4 w-36" />
+        </div>
+      </section>
     </div>
   );
 }
--- src/lib/getDataMax4.ts
+++ src/lib/getDataMax4.ts
@@ -2,7 +2,7 @@ import { AirtableRecord, AirtableResponse } from "@/interfaces";
 
 export const getDataMax4 = async (): Promise<AirtableRecord[]> => {
   const url =
-    "https://api.airtable.com/v0/appZPb0bGSr4922QN/Posters?maxRecords=4&view=Grid%20view";
+    "https://api.airtable.com/v0/appZPb0bGSr4922QN/Posters?maxRecords=8&view=Grid%20view";
   const headers = {
     Authorization:
       "Bearer patUv0gZATPbSw2xA.4b1b2eaa3f4099122601e14f54d018f9de2d7fba01693e8eb6a5f7dee3567632",
