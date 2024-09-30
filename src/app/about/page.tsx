222d20f1e08e8da1fe62f1f5aa0ba882f962f973
Tim Spiridonov <timour.spiridonov0gmail.com>
2024-09-30 15:30:10 +0100

    final modification sabrina

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,7 +13,7 @@ export default async function About() {
   return (
     <div>
       <section className="">
-      <div className="relative py-12 lg:py-48 items-center">
+        <div className="relative py-12 lg:py-48 items-center">
           <div className="relative flex items-center px-12 justify-center h-full z-20">
             <h1 className="text-center text-3xl md:text-5xl text-black font-bold plexSans">
               What is the Behna Cinema Archive?
@@ -28,24 +28,61 @@ export default async function About() {
             {/* <h2 className="text-3xl font-bold mb-4 text-gray-900 font-ibmSans">
         What are Behna Archives?
       </h2> */}
-<p className="py-2 font-ibmSans">
-  Moving images captivated the world in the early 1900s, marking the emergence of cinema as a global cultural phenomenon.
-</p>
-<p className="py-2 font-ibmSans">
-  In the 1920s, Alexandria saw the founding of Sélections Behna Films by three brothers, Edouard, Michel and Georges Behna, just as the city was becoming a centre for Egypt&#8217;s burgeoning film industry. An historic centre of scholarship, trade, and culture since the Ptolemaic dynasty, Alexandria regained its status as the pride of the Mediterranean and a hub for the arts following a cultural renaissance in the 19th century. This thriving trading post, inhabited by a cosmopolitan mosaic of cultures, became a focal point for the emerging film industry.
-</p>
-<p className="py-2 font-ibmSans">
-  In 1932, the Behna brothers co-produced &#8216;Onshoudat al-Fouad&#8217;, with Nahas Sphynx Film, the first Egyptian musical film, and in 1936, they launched into film distribution and distributed in the same year the first Egyptian animated film &#8216;Mish Mish Effendi,&#8217; before transitioning to film distribution. They quickly became the main distributors of Egyptian films in the Arab world, with offices in Alexandria, Cairo, Baghdad, Khartoum, Beirut and Damascus. Sélections Behna Films participated in many iconic films in the history of Egyptian cinema, including works by Assia, Marie-Queen, Ahmed Galal, comedy icon Ismail Yassin and composer-actor Mohamed Fawzy.
-</p>
-<p className="py-2 font-ibmSans">
-  Following the 1952 revolution and the widespread campaign of nationalisation, Sélections Behna Films was compelled to cease operations in 1961. After prolonged legal battles, the heirs to this former cinematic empire, Basile Behna and his sisters, sought to reclaim the assets of their expropriated company. During this time, Basile Behna, as a prominent art collector who possessed a diverse art collection, embarked on building his own cinema collection that chronicled his family&#8217;s rich heritage. He meticulously acquired pieces from antiquities markets and other collectors to reflect the long and storied history of Sélections Behna Films.
-</p>
-<p className="py-2 font-ibmSans">
-  A collaboration between Wekalet Behna (Basile Behna), Community Jameel and ARCHiVe (Factum Foundation and Fondazione Giorgio Cini), the Behna Cinema Archive began as a digitisation project to preserve Wekalet Behna&#8217;s treasured early 20th-century poster collection. Today it is a platform dedicated to celebrating and preserving Egyptian cinema history.
-</p>
-<p className="py-2 font-ibmSans">
-  The collection offers a rare and precious testimony to the golden era of Egyptian cinema. As an accessible, digital archive, it aims to serve as a tool for film lovers, producers, researchers and graphic designers globally.
-</p>
+            <p className="py-2 font-ibmSans">
+              Moving images captivated the world in the early 1900s, marking the
+              emergence of cinema as a global cultural phenomenon.
+            </p>
+            <p className="py-2 font-ibmSans">
+              In the 1920s, Alexandria saw the founding of Sélections Behna
+              Films by three brothers, Edouard, Michel and George Behna, just as
+              the city was becoming a centre for Egypt&#8217;s burgeoning film
+              industry. An historic centre of scholarship, trade, and culture
+              since the Ptolemaic dynasty, Alexandria regained its status as the
+              pride of the Mediterranean and a hub for the arts following a
+              cultural renaissance in the 19th century. This thriving trading
+              post, inhabited by a cosmopolitan mosaic of cultures, became a
+              focal point for the emerging film industry.
+            </p>
+            <p className="py-2 font-ibmSans">
+              In 1932, the Behna brothers co-produced &#8216;Onshoudat
+              al-Fouad&#8217;, with Nahas Sphynx Film, the first Egyptian
+              musical film, and in 1936, they launched into film distribution
+              and distributed in the same year the first Egyptian animated film
+              &#8216;Mish Mish Effendi,&#8217; before transitioning to film
+              distribution. They quickly became the main distributors of
+              Egyptian films in the Arab world, with offices in Alexandria,
+              Cairo, Baghdad, Khartoum, Beirut and Damascus. Sélections Behna
+              Films participated in many iconic films in the history of Egyptian
+              cinema, including works by Assia Dagher, Marie-Queen, Ahmed Galal, comedy
+              icon Ismail Yassin and composer-actor Mohamed Fawzi.
+            </p>
+            <p className="py-2 font-ibmSans">
+              Following the 1952 revolution and the widespread campaign of
+              nationalisation, Sélections Behna Films was compelled to cease
+              operations in 1961. After prolonged legal battles, the heirs to
+              this former cinematic empire, Basile Behna and his sisters, sought
+              to reclaim the assets of their expropriated company. During this
+              time, Basile Behna, as a prominent art collector who possessed a
+              diverse art collection, embarked on building his own cinema
+              collection that chronicled his family&#8217;s rich heritage. He
+              meticulously acquired pieces from antiquities markets and other
+              collectors to reflect the long and storied history of Sélections
+              Behna Films.
+            </p>
+            <p className="py-2 font-ibmSans">
+              A collaboration between Wekalet Behna (Basile Behna), Community
+              Jameel and ARCHiVe (Factum Foundation and Fondazione Giorgio
+              Cini), the Behna Cinema Archive began as a digitisation project to
+              preserve Wekalet Behna&#8217;s treasured early 20th-century poster
+              collection. Today it is a platform dedicated to celebrating and
+              preserving Egyptian cinema history.
+            </p>
+            <p className="py-2 font-ibmSans">
+              The collection offers a rare and precious testimony to the golden
+              era of Egyptian cinema. As an accessible, digital archive, it aims
+              to serve as a tool for film lovers, producers, researchers and
+              graphic designers globally.
+            </p>
             <div className="w-auto"></div>
           </div>
           <div className="column-image hidden md:block md:align-top">
@@ -58,15 +95,15 @@ export default async function About() {
         </div>
         <div className="px-24 border-b border-gray-300 py-6"></div>
       </section>
-      
+
       <section className="px-4 md:max-w-[1700px] mx-auto pb-12">
         <div className="column-image hidden md:block">
-            <img
-              src="https://storage.googleapis.com/behna-archives/267_GS_1955_For%20the%20Sake%20of%20Love.webp"
-              alt=""
-              className="self-end"
-            />
-          </div>
+          <img
+            src="https://storage.googleapis.com/behna-archives/267_GS_1955_For%20the%20Sake%20of%20Love.webp"
+            alt=""
+            className="self-end"
+          />
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-1 md:gap-12= md:align-top">
           <div className="text-column">
             <h2 className="text-3xl font-bold m-0 text-gray-900 font-ibmSans mb-2">
@@ -141,7 +178,6 @@ export default async function About() {
 
             <div className="w-auto"></div>
           </div>
-       
         </div>
         <div className="border-b border-gray-300 py-6"></div>
       </section>
@@ -151,40 +187,38 @@ export default async function About() {
           Supported by
         </h2>
         <div className="flex flex-col justify-start items-start md:flex-row md:space-x-12">
-  <Link href="https://www.behna.org/en/" target="_blank">
-    <Image 
-      src={behnaLogo} 
-      alt="BEHNA Logo" 
-      className="py-4 w-36 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
+          <Link href="https://www.behna.org/en/" target="_blank">
+            <Image
+              src={behnaLogo}
+              alt="BEHNA Logo"
+              className="py-4 w-36 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
 
-  <Link href="https://www.communityjameel.org/" target="_blank">
-    <Image 
-      src={cjLogo} 
-      alt="Community Jameel Logo" 
-      className="py-4 w-48 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
+          <Link href="https://www.communityjameel.org/" target="_blank">
+            <Image
+              src={cjLogo}
+              alt="Community Jameel Logo"
+              className="py-4 w-48 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
 
-  <Link href="https://factumfoundation.org/" target="_blank">
-    <Image 
-      src={factumLogo} 
-      alt="FACTUM Logo" 
-      className="py-4 w-64 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
+          <Link href="https://factumfoundation.org/" target="_blank">
+            <Image
+              src={factumLogo}
+              alt="FACTUM Logo"
+              className="py-4 w-64 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
 
-  <Link href="https://www.cini.it/" target="_blank">
-    <Image 
-      src={fonazione} 
-      alt="Fonazione Cini Logo" 
-      className="py-4 w-52 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
-</div>
-
-
+          <Link href="https://www.cini.it/" target="_blank">
+            <Image
+              src={fonazione}
+              alt="Fonazione Cini Logo"
+              className="py-4 w-52 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
+        </div>
       </section>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,52 +31,51 @@ export default async function Home() {
           </div>
         </div>
         <div className="w-full px-4 md:max-w-[1700px] items-center mx-auto   ">
-        <div className="masonry-grid" style={{ maxWidth: "100%" }}>
-  {/* Mobile View */}
-  <div className="block md:hidden">
-    {imagesHome
-      .filter((image, index) => index > 3)
-      .map((image) => (
-        <Link
-          key={image.id}
-          href={`collection/${image.slug}`}
-          className="masonry-item group block"
-        >
-          <div className="relative w-full overflow-hidden">
-            <img
-              src={image.url}
-              alt={image.slug}
-              className="w-full h-auto object-cover object-center transition-all duration-[2000ms] ease-in-out group-hover:blend-multiply"
-            />
-            {/* Gradient background with slow transition */}
-            <div className="absolute inset-0 bg-blue-900 opacity-0 group-hover:opacity-25 transition-opacity duration-[2000ms] ease-in-out pointer-events-none"></div>
+          <div className="masonry-grid" style={{ maxWidth: "100%" }}>
+            {/* Mobile View */}
+            <div className="block md:hidden">
+              {imagesHome
+                .filter((image, index) => index > 3)
+                .map((image) => (
+                  <Link
+                    key={image.id}
+                    href={`collection/${image.slug}`}
+                    className="masonry-item group block"
+                  >
+                    <div className="relative w-full overflow-hidden">
+                      <img
+                        src={image.url}
+                        alt={image.slug}
+                        className="w-full h-auto object-cover object-center transition-all duration-[2000ms] ease-in-out group-hover:blend-multiply"
+                      />
+                      {/* Gradient background with slow transition */}
+                      <div className="absolute inset-0 bg-blue-900 opacity-0 group-hover:opacity-25 transition-opacity duration-[2000ms] ease-in-out pointer-events-none"></div>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+
+            {/* Desktop View */}
+            <div className="hidden md:block">
+              {imagesHome.map((image) => (
+                <Link
+                  key={image.id}
+                  href={`collection/${image.slug}`}
+                  className="masonry-item group block"
+                >
+                  <div className="relative w-full overflow-hidden">
+                    <img
+                      src={image.url}
+                      alt={image.slug}
+                      className="w-full h-auto object-cover object-center transition-all duration-[2000ms] ease-in-out group-hover:blend-multiply"
+                    />
+                    {/* Gradient background with slow transition */}
+                    <div className="absolute inset-0 bg-blue-900 opacity-0 group-hover:opacity-25 transition-opacity duration-[2000ms] ease-in-out pointer-events-none"></div>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
-        </Link>
-      ))}
-  </div>
-
-  {/* Desktop View */}
-  <div className="hidden md:block">
-    {imagesHome.map((image) => (
-      <Link
-        key={image.id}
-        href={`collection/${image.slug}`}
-        className="masonry-item group block"
-      >
-        <div className="relative w-full overflow-hidden">
-          <img
-            src={image.url}
-            alt={image.slug}
-            className="w-full h-auto object-cover object-center transition-all duration-[2000ms] ease-in-out group-hover:blend-multiply"
-          />
-          {/* Gradient background with slow transition */}
-          <div className="absolute inset-0 bg-blue-900 opacity-0 group-hover:opacity-25 transition-opacity duration-[2000ms] ease-in-out pointer-events-none"></div>
-        </div>
-      </Link>
-    ))}
-  </div>
-</div>
-
         </div>
       </section>
       <section className=" mx-auto">
@@ -91,30 +90,67 @@ export default async function Home() {
         </div>
       </section>
       <section className="max-w-[1700px] mx-auto">
-      <div className="mx-4 px-24 border-b border-gray-300"></div>
+        <div className="mx-4 px-24 border-b border-gray-300"></div>
         <div className="grid grid-cols-1 px-4 lg:px-4 py-12 md:grid-cols-2 gap-4">
           <div className="text-column">
             <h2 className="text-3xl font-bold m-0 text-gray-900 font-ibmSans md:pb-2">
               What is the Behna Cinema Archive
             </h2>
             <p className="py-2 font-ibmSans">
-  Moving images captivated the world in the early 1900s, marking the emergence of cinema as a global cultural phenomenon.
-</p>
-<p className="py-2 font-ibmSans">
-  In the 1920s, Alexandria saw the founding of Sélections Behna Films by three brothers, Edouard, Michel and Georges Behna, just as the city was becoming a centre for Egypt&#8217;s burgeoning film industry. An historic centre of scholarship, trade, and culture since the Ptolemaic dynasty, Alexandria regained its status as the pride of the Mediterranean and a hub for the arts following a cultural renaissance in the 19th century. This thriving trading post, inhabited by a cosmopolitan mosaic of cultures, became a focal point for the emerging film industry.
-</p>
-<p className="py-2 font-ibmSans">
-  In 1932, the Behna brothers co-produced &#8216;Onshoudat al-Fouad&#8217;, with Nahas Sphynx Film, the first Egyptian musical film, and in 1936, they launched into film distribution and distributed in the same year the first Egyptian animated film &#8216;Mish Mish Effendi,&#8217; before transitioning to film distribution. They quickly became the main distributors of Egyptian films in the Arab world, with offices in Alexandria, Cairo, Baghdad, Khartoum, Beirut and Damascus. Sélections Behna Films participated in many iconic films in the history of Egyptian cinema, including works by Assia, Marie-Queen, Ahmed Galal, comedy icon Ismail Yassin and composer-actor Mohamed Fawzy.
-</p>
-<p className="py-2 font-ibmSans">
-  Following the 1952 revolution and the widespread campaign of nationalisation, Sélections Behna Films was compelled to cease operations in 1961. After prolonged legal battles, the heirs to this former cinematic empire, Basile Behna and his sisters, sought to reclaim the assets of their expropriated company. During this time, Basile Behna, as a prominent art collector who possessed a diverse art collection, embarked on building his own cinema collection that chronicled his family&#8217;s rich heritage. He meticulously acquired pieces from antiquities markets and other collectors to reflect the long and storied history of Sélections Behna Films.
-</p>
-<p className="py-2 font-ibmSans">
-  A collaboration between Wekalet Behna (Basile Behna), Community Jameel and ARCHiVe (Factum Foundation and Fondazione Giorgio Cini), the Behna Cinema Archive began as a digitisation project to preserve Wekalet Behna&#8217;s treasured early 20th-century poster collection. Today it is a platform dedicated to celebrating and preserving Egyptian cinema history.
-</p>
-<p className="py-2 font-ibmSans">
-  The collection offers a rare and precious testimony to the golden era of Egyptian cinema. As an accessible, digital archive, it aims to serve as a tool for film lovers, producers, researchers and graphic designers globally.
-</p>
+              Moving images captivated the world in the early 1900s, marking the
+              emergence of cinema as a global cultural phenomenon.
+            </p>
+            <p className="py-2 font-ibmSans">
+              In the 1920s, Alexandria saw the founding of Sélections Behna
+              Films by three brothers, Edouard, Michel and George Behna, just
+              as the city was becoming a centre for Egypt&#8217;s burgeoning
+              film industry. An historic centre of scholarship, trade, and
+              culture since the Ptolemaic dynasty, Alexandria regained its
+              status as the pride of the Mediterranean and a hub for the arts
+              following a cultural renaissance in the 19th century. This
+              thriving trading post, inhabited by a cosmopolitan mosaic of
+              cultures, became a focal point for the emerging film industry.
+            </p>
+            <p className="py-2 font-ibmSans">
+              In 1932, the Behna brothers co-produced &#8216;Onshoudat
+              Al Fouad&#8217;, with Nahas Sphynx Film, the first Egyptian
+              musical film, and in 1936, they launched into film distribution
+              and distributed in the same year the first Egyptian animated film
+              &#8216;Mish Mish Effendi,&#8217; before transitioning to film
+              distribution. They quickly became the main distributors of
+              Egyptian films in the Arab world, with offices in Alexandria,
+              Cairo, Baghdad, Khartoum, Beirut and Damascus. Sélections Behna
+              Films participated in many iconic films in the history of Egyptian
+              cinema, including works by Assia Dagher, Marie-Queen, Ahmed Galal, comedy
+              icon Ismail Yassin and composer-actor Mohamed Fawzi.
+            </p>
+            <p className="py-2 font-ibmSans">
+              Following the 1952 revolution and the widespread campaign of
+              nationalisation, Sélections Behna Films was compelled to cease
+              operations in 1961. After prolonged legal battles, the heirs to
+              this former cinematic empire, Basile Behna and his sisters, sought
+              to reclaim the assets of their expropriated company. During this
+              time, Basile Behna, as a prominent art collector who possessed a
+              diverse art collection, embarked on building his own cinema
+              collection that chronicled his family&#8217;s rich heritage. He
+              meticulously acquired pieces from antiquities markets and other
+              collectors to reflect the long and storied history of Sélections
+              Behna Films.
+            </p>
+            <p className="py-2 font-ibmSans">
+              A collaboration between Wekalet Behna (Basile Behna), Community
+              Jameel and ARCHiVe (Factum Foundation and Fondazione Giorgio
+              Cini), the Behna Cinema Archive began as a digitisation project to
+              preserve Wekalet Behna&#8217;s treasured early 20th-century poster
+              collection. Today it is a platform dedicated to celebrating and
+              preserving Egyptian cinema history.
+            </p>
+            <p className="py-2 font-ibmSans">
+              The collection offers a rare and precious testimony to the golden
+              era of Egyptian cinema. As an accessible, digital archive, it aims
+              to serve as a tool for film lovers, producers, researchers and
+              graphic designers globally.
+            </p>
 
             <div className="w-auto">
               <Link href={"/about"}>
@@ -147,40 +183,38 @@ export default async function Home() {
           Supported by
         </h2>
         <div className="flex flex-col justify-start items-start md:flex-row md:space-x-12">
-  <Link href="https://www.behna.org/en/" target="_blank">
-    <Image 
-      src={behnaLogo} 
-      alt="BEHNA Logo" 
-      className="py-4 w-36 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
+          <Link href="https://www.behna.org/en/" target="_blank">
+            <Image
+              src={behnaLogo}
+              alt="BEHNA Logo"
+              className="py-4 w-36 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
 
-  <Link href="https://www.communityjameel.org/" target="_blank">
-    <Image 
-      src={cjLogo} 
-      alt="Community Jameel Logo" 
-      className="py-4 w-48 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
+          <Link href="https://www.communityjameel.org/" target="_blank">
+            <Image
+              src={cjLogo}
+              alt="Community Jameel Logo"
+              className="py-4 w-48 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
 
-  <Link href="https://factumfoundation.org/" target="_blank">
-    <Image 
-      src={factumLogo} 
-      alt="FACTUM Logo" 
-      className="py-4 w-64 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
+          <Link href="https://factumfoundation.org/" target="_blank">
+            <Image
+              src={factumLogo}
+              alt="FACTUM Logo"
+              className="py-4 w-64 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
 
-  <Link href="https://www.cini.it/" target="_blank">
-    <Image 
-      src={fonazione} 
-      alt="Fonazione Cini Logo" 
-      className="py-4 w-52 transition-opacity duration-300 hover:opacity-60" 
-    />
-  </Link>
-</div>
-
-
+          <Link href="https://www.cini.it/" target="_blank">
+            <Image
+              src={fonazione}
+              alt="Fonazione Cini Logo"
+              className="py-4 w-52 transition-opacity duration-300 hover:opacity-60"
+            />
+          </Link>
+        </div>
       </section>
     </div>
   );
